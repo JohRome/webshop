32fa5fb2e2df4fd359d6aaf609dcdfc5042738f6
Sandra1887 <93sanjep@molndal.gafe.se>
2023-12-07 13:18:22 +0100

    created ProductController

---
+++ src/main/java/com/temp/webshop/webshop/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.temp.webshop.webshop.controller;
+
+import com.temp.webshop.webshop.entity.Product;
+import com.temp.webshop.webshop.repository.ProductRepository;
+import com.temp.webshop.webshop.service.ProductService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("")
+    public ResponseEntity<Product> addProduct(Product product) {
+        return ResponseEntity.ok(productService.saveProduct(product));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> findOneProduct(
+            @PathVariable Long id) {
+        Optional<Product> product = productService.getOneProduct(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> findAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Long id,
+            @RequestBody Product product
+    ) {
+        return ResponseEntity.ok(productService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(
+            @PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok("Product deleted");
+    }
+ }
